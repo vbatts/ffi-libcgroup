32891506c0e1b6b52a9c081e60e8f4d8ecd0e926
Vincent Batts <vbatts@thisco.de>
2013-12-16 12:13:32 -0500

    Wrapping the two init functions, to feel more rubyish

--- lib/libcgroup/cg.rb
+++ lib/libcgroup/cg.rb
@@ -10,9 +10,41 @@ module LibCgroup
 
   CGFLAG_USECACHE = 0x01
 
+  # The application must initialize libcgroup using LibCgroup.init before any
+  # other libcgroup function can be called. libcgroup caches information
+  # about mounted hierarchies (just what's mounted where, not the control groups
+  # themselves) at this time. There is currently no way to refresh this cache,
+  # i.e. all subsequent mounts/remounts/unmounts are not reflected in this cache
+  # and libcgroup may produce unexpected results.
+  def self.init
+    self.cgroup_init
+  end
+
+  # Returns path where is mounted given controller.
+  # Only the first mount point is returned, use
+  # LibCgroup.get_subsys_mount_point_begin(), LibCgroup.get_subsys_mount_point_next()
+  # and LibCgroup.get_subsys_mount_point_end() to get all of them.
+  #
+  # controller Name of the controller
+  #
+  #   LibCgroup.init
+  #    => 0 
+  #   LibCgroup.get_subsys_mount_point("cpu")
+  #    => "/sys/fs/cgroup" 
+  #
+  def self.get_subsys_mount_point(ctl)
+    objptr = FFI::MemoryPointer.new :pointer
+    self.cgroup_get_subsys_mount_point(ctl, objptr)
+    strPtr = objptr.read_pointer
+    return strPtr.null? ? "" : strPtr.read_string
+  end
+
   # init.h
   attach_function :cgroup_init, [], :int
   attach_function :cgroup_get_subsys_mount_point, [:string, :pointer], :int
+
+  # iterators.h
+  #attach_function :cgroup_get_all_controller_begin, [void **handle, struct controller_data *info], :int
 
   # tasks.h
   attach_function :cgroup_attach_task, [:cgroup], :int
